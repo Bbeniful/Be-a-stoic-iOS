012b0cd84b4999ef36a1f547fadb4e3fa1492182
Bbeniful <bbenikeszi@gmail.com>
2024-03-08 17:17:28 +0100

    Initial Commit

---
+++ Be a Stoic/Be_a_StoicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Be_a_StoicApp.swift
+//  Be a Stoic
+//
+//  Created by Benjámin Szilágyi on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Be_a_StoicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Be a StoicUITests/Be_a_StoicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Be_a_StoicUITestsLaunchTests.swift
+//  Be a StoicUITests
+//
+//  Created by Benjámin Szilágyi on 08/03/2024.
+//
+
+import XCTest
+
+final class Be_a_StoicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
